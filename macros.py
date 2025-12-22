19fc2355b494f4b6526ef68e699ba57dbcc0d337
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-12-22 14:49:36 -0600

    [UPDATE] Format macros.py with ruff

--- macros.py
+++ macros.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S python3 -O
 """
 tmux-macros: A tool to run macros in tmux using Python scripts.
 This script allows you to run predefined macros in tmux panes.
 It reads macros from a YAML file, generates a cache, and allows execution of macros via command line arguments.
 """
 
-import os
 import argparse
+import os
 import subprocess
 from time import sleep
+
 from utils import load_conf, parse_macros_yml_and_generate_cache, tmux_print
 
 
 def get_active_pane():
-    return subprocess.check_output([
-        "tmux", "display-message", "-p", "#{pane_id}"
-    ]).decode("utf-8").strip()
+    return (
+        subprocess.check_output(["tmux", "display-message", "-p", "#{pane_id}"])
+        .decode("utf-8")
+        .strip()
+    )
 
 
 def send_command_to_pane(pane_id, command):
@@ -29,17 +32,21 @@ def run_macro(macros_dict, macro_name):
         tmux_print(f"Macro '{macro_name}' not found.")
         return
 
-    for c in macros_dict[macro_name]['commands']:
-        if c['type'] == 'text' or c['type'] == 'keypress':
-            send_command_to_pane(active_pane, c['value'])
-        elif c['type'] == 'sleep':
-            sleep(0.001 * int(c['value']))
+    for c in macros_dict[macro_name]["commands"]:
+        if c["type"] == "text" or c["type"] == "keypress":
+            send_command_to_pane(active_pane, c["value"])
+        elif c["type"] == "sleep":
+            sleep(0.001 * int(c["value"]))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("macro", nargs="?", help="Macro to execute")
-    parser.add_argument("--update-cache", action="store_true", help="Regenerate macros_cache.py and .tmux.macros.conf")
+    parser.add_argument(
+        "--update-cache",
+        action="store_true",
+        help="Regenerate macros_cache.py and .tmux.macros.conf",
+    )
     args = parser.parse_args()
 
     conf = load_conf()
@@ -48,12 +55,13 @@ def main():
         parse_macros_yml_and_generate_cache(conf)
         return
 
-    if not os.path.exists(conf['macros_cache_py']):
+    if not os.path.exists(conf["macros_cache_py"]):
         tmux_print("⚠️ Cache not found. Regenerating...")
         parse_macros_yml_and_generate_cache(conf)
 
-    from importlib.util import spec_from_file_location, module_from_spec
-    spec = spec_from_file_location("macros_cache", conf['macros_cache_py'])
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location("macros_cache", conf["macros_cache_py"])
     macros_cache = module_from_spec(spec)
     spec.loader.exec_module(macros_cache)
     macros_dict = macros_cache.MACROS
